f60c61b4409b4cf86444d6797d82fcd39e987426
Matthew Grant <mgrant.xi@gmail.com>
2023-04-13 19:08:49 -0700

    added all SQL tables with content (quizzes, answers, etc)

---
+++ sql/Quiz_202304131906.sql
@@ -0,0 +1,14 @@
+INSERT INTO Quiz (name,description,image_location,category_id) VALUES
+	 ('World History Basics','Test your knowledge on important events from world history','',1),
+	 ('US History Highlights','Discover how well you know key moments in American history','',1),
+	 ('Ancient Civilizations','Explore the ancient world and its fascinating civilizations','',1),
+	 ('Countries and Capitals','Identify countries and their capitals from around the world','',2),
+	 ('Famous Landmarks','Recognize famous landmarks and their locations','',2),
+	 ('Geographical Wonders','Challenge your knowledge of the world’s natural wonders','',2),
+	 ('Biology Fundamentals','Answer questions about the basics of biology','',3),
+	 ('Intro to Chemistry','Test your understanding of introductory chemistry concepts','',3),
+	 ('Physics Principles','Examine your grasp of fundamental physics principles','',3),
+	 ('USC History','Test your knowledge of the history of the University of Southern California','',8);
+INSERT INTO Quiz (name,description,image_location,category_id) VALUES
+	 ('USC Campus','Discover how well you know the USC campus and its landmarks','',8),
+	 ('USC Athletics','Challenge your knowledge of USC sports teams and achievements','',8);
