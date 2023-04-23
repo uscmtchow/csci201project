e6e35e5fd13d16a21280f6afb4d70cdf806edf57
Evan2 <Evan2@Evan-Macbook.local>
2023-04-23 11:29:13 -0700

    Added getCookie() in login.js

--- CSCI201Quiz/src/main/webapp/js/login.js
+++ CSCI201Quiz/src/main/webapp/js/login.js
@@ -36,3 +36,30 @@
 })(jQuery);
 
 
+$(document).ready(function () {
+
+    function getCookie(cname) {
+        let name = cname + "=";
+        let decodedCookie = decodeURIComponent(document.cookie);
+        let ca = decodedCookie.split(';');
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    }
+    function deleteCookie(cname) {
+        document.cookie = cname + '=; Max-Age=-99999999;' + 'domain=localhost';
+        updateNavByLoginStatus();
+    }
+    function logOut() {
+        deleteCookie("username");
+        console.log(getCookie("username"));
+        window.location.href = "index.html";
+    }
+});
